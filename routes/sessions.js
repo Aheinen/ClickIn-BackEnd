1c15bd2e7ae7c16ed86aeec4ac329268dd36b8e5
Sebastien Cheraux <scheraux@gmail.com>
2015-08-16 12:02:56 -0700

    Add session patch route

--- routes/sessions.js
+++ routes/sessions.js
@@ -24,6 +24,28 @@ router.get('/:session_code', function(req, res, next) {
 
 });
 
+// Update session by code
+router.patch('/:session_code', function(req, res, next) {
+
+  Session.find({ code: req.params.session_code }, function(err, sessions) {
+    if (err) throw err;
+
+    for (var i = 0; i < sessions[0].poll.answers.length; i++) {
+      if (sessions[0].poll.answers[i].id === req.body.id) {
+        sessions[0].poll.answers[i].count++;
+      }
+    }
+
+    sessions[0].save(function(err) {
+      if (err) throw err;
+
+      res.json(sessions[0]);
+    });
+
+  });
+
+});
+
 // Post new session
 router.post('/', function(req, res, next) {
 
